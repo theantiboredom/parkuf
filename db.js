bc12618027b2bf74545154aebab124ce58340665
Travis <lightus@live.com>
2015-12-15 10:46:31 +0000

    changed db url

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/ParkUF');
+mongoose.connect('mongodb://main:moasdf123@ec2-52-23-224-82.compute-1.amazonaws.com:27017/dummyDB');
 
 module.exports = mongoose.connection;
