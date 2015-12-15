446dd1b7f4bbb9957d806c15d999b22b7ea40bdd
Travis Johnson <lightus@ufl.edu>
2015-12-15 02:05:25 -0500

    Full server
    Full server

---
+++ data/index.js
@@ -0,0 +1,74 @@
+module.exports = {
+	"31": {
+		"number": 31,
+		"name": "Flavet Parking",
+		"decal": "Red",
+		"spotsOpen": 27,
+		"spotsTotal": 210,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	},
+	"32": {
+		"number": 32,
+		"name": "Reitz Union Parking",
+		"decal": "Gated,Visitors",
+		"spotsOpen": 12,
+		"spotsTotal": 271,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	},
+	"33": {
+		"number": 33,
+		"name": "Shands Parking",
+		"decal": "Yellow,Gated,Visitor",
+		"spotsOpen": 27,
+		"spotsTotal": 780,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	},
+	"34": {
+		"number": 34,
+		"name": "Cancer Center Parking",
+		"decal": "Visitor",
+		"spotsOpen": 100,
+		"spotsTotal": 250,
+		"beginEnforcement": "Open All Times",
+		"endEnforcement": "Open All Times"
+	},
+	"35": {
+		"number": 35,
+		"name": "Fifield Hall Parking",
+		"decal": "Orange",
+		"spotsOpen": 10,
+		"spotsTotal": 502,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	},
+	"36": {
+		"number": 36,
+		"name": "Criser Hall Parking",
+		"decal": "Red",
+		"spotsOpen": 22,
+		"spotsTotal": 120,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	},
+	"37": {
+		"number": 37,
+		"name": "Murphree Parking",
+		"decal": "Red",
+		"spotsOpen": 17,
+		"spotsTotal": 50,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	},
+	"38": {
+		"number": 38,
+		"name": "Marston Parking",
+		"decal": "Yellow",
+		"spotsOpen": 11,
+		"spotsTotal": 150,
+		"beginEnforcement": "7:30AM",
+		"endEnforcement": "3:30PM"
+	}
+};
